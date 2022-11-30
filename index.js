392d4989f222480712e46776ed33c88a0497e844
Ahsan Nawaz <63430184+AhsanNawaz1@users.noreply.github.com>
2022-11-30 10:30:57 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+// const bodyParser = require("body-parser");
+const express = require("express");
+const userRoute = require("./routes/user");
+const DB = require("./Database/config");
+const allRoutes = require("./routes/index");
+const CRUDSchema = require("./routes/CRUD");
+const route = require("./routes/index");
+const { fileParser } = require('express-multipart-file-parser')
+
+
+const database = async () => {
+  await DB();
+}
+
+database();
+
+const app = express();
+var cors = require('cors');
+app.use(cors())
+const { API_PORT } = process.env;
+const PORT = 5000;
+
+app.use(express.json());
+// app.use(bodyParser.urlencoded({
+//   extended: true
+// }));
+app.use(fileParser({
+  rawBodyOptions: {
+    limit: '15mb',
+  },
+}))
+app.use(route);
+app.listen(PORT, () => console.log("server Running"));
+
