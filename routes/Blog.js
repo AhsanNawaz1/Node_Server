ef3ecef701a1f9b613e917940c93e35010e655c1
Ahsan Nawaz <63430184+AhsanNawaz1@users.noreply.github.com>
2022-12-05 12:28:27 +0500

    Update Blog.js

--- routes/Blog.js
+++ routes/Blog.js
@@ -1,6 +1,7 @@
 const bodyParser = require("body-parser");
 const { application } = require("express");
 const express = require("express");
+const crypto = require('crypto');
 const userModal = require("../Model/user")
 var bcrypt = require('bcryptjs');
 var jwt = require("jsonwebtoken");
@@ -10,73 +11,145 @@ const Posts = require("../Model/BlogSchema");
 const router = express.Router();
 const multer = require('multer')
 const upload = multer({ dest: 'uploads/' })
+require("dotenv").config();
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const { S3Client, PutObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { parse } = require("path");
+const generateFileName = (bytes = 32) => crypto.randomBytes(bytes).toString('hex')
+
+
+const BUCKET_NAME = process.env.BUCKET_NAME;
+
+const BUCKET_REGION = process.env.BUCKET_REGION;
+
+const ACCESS_KEY = process.env.ACCESS_KEY;
+
+const SECRET_ACCESS_KEY = process.env.SECRET_ACCESS_KEY;
+
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: ACCESS_KEY,
+    secretAccessKey: SECRET_ACCESS_KEY,
+  },
+  region: BUCKET_REGION
+})
+
+router.post("/create", async (req, res) => {
+
+  try {
+
+    const {
+      title,
+      description,
+      img,
+      topic,
+      categories,
+      tags,
+      author,
+      authorID
+    } = req.body
+
+    const imageName = generateFileName()
+
+    const params = {
+      Bucket: BUCKET_NAME,
+      Key: imageName,
+      Body: req.files[0].buffer,
+      ContentType: req.files[0].mimetype
+    }
 
 
 
-router.post('/create', upload.single('uploaded_file'), function (req, res) {
-  // req.file is the name of your file in the form above, here 'uploaded_file'
-  // req.body will hold the text fields, if there were any 
-  console.log(req.file, req.body)
+    const command = new PutObjectCommand(params)
+
+    await s3.send(command);
+
+    const getCommand = new GetObjectCommand({
+      Bucket: BUCKET_NAME,
+      Key: imageName
+    })
+
+    const url = await getSignedUrl(
+      s3,
+      getCommand)
+
+    const obj = {
+      title: title,
+      description: description,
+      imgName: imageName,
+      imgURL: url,
+      topic: topic,
+      categories: JSON.parse(categories),
+      tags: JSON.parse(tags),
+      author: author,
+      authorID: authorID,
+    }
+
+    const posts = await Posts.create(obj);
+
+    const allPosts = await Posts.find({});
+
+    return res.status(200).send(allPosts);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
+router.get("/getPosts", async (req, res) => {
 
-// router.post("/create", async (req, res) => {
-//   console.log("req===> ", req.files);
-//   return res.status(200).send(req.files)
-//   // try {
+  try {
 
-//   //   const {
-//   //     title,
-//   //     description,
-//   //     img,
-//   //     topic,
-//   //     categories,
-//   //     tags,
-//   //     author
-//   //   } = req.body
+    const allPosts = await Posts.find({});
 
-//   //   console.log("req", req.body)
+    const posts = JSON.stringify(allPosts);
+    let postsParse = JSON.parse(posts);
+    let arr = []
 
+    for (let i = 0; i < postsParse.length; i++) {
+      let temp = await gettingURL(postsParse[i].imgName);
+      postsParse[i].url = temp
+    };
 
-//   //   if (!(title &&
-//   //     description &&
-//   //     img &&
-//   //     topic &&
-//   //     categories &&
-//   //     tags &&
-//   //     author)) {
-//   //     return res.status(400).send("All input is required");
-//   //   }
+    console.log("postsParse", postsParse)
 
-//   //   const posts = await Posts.create({
-//   //     title,
-//   //     description,
-//   //     img,
-//   //     topic,
-//   //     categories,
-//   //     tags,
-//   //     author
-//   //   });
+    async function gettingURL(name) {
+      const getCommand = new GetObjectCommand({
+        Bucket: BUCKET_NAME,
+        Key: name
+      })
+
+      let url = await getSignedUrl(
+        s3,
+        getCommand);
+
+      return url
+    }
+
+    // JSON.parse(posts).forEach(async (i) => {
+    //   const getCommand = new GetObjectCommand({
+    //     Bucket: BUCKET_NAME,
+    //     Key: i.imgName
+    //   })
 
 
+    //   url = await getSignedUrl(
+    //     s3,
+    //     getCommand);
 
-//   //   return res.status(200).send(posts);
-//   // } catch (err) {
-//   //   console.log(err);
-//   // }
-// });
 
-// router.get("/getCRUD", async (req, res) => {
+    //   i['url'] = url;
 
-//   try {
+    //   arr.push(i)
+    // });
 
-//     const crudTable = await CRUD.find({})
 
-//     res.status(201).json(crudTable);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
+
+
+    res.status(201).json(postsParse);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 // router.post("/update/:id", async (req, res) => {
 
